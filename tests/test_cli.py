bc8e2ec4aba04f3d7253a03e2c2ce4a32d57f926
Brandon Castellano <brandon248@gmail.com>
2021-10-29 23:11:33 -0400

    Improve readability of CLI tests.
    Add helper function which both invokes the scenedetect command,
    as well as accepts a format string with some defaults to help
    build command chains.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -28,73 +28,88 @@ import subprocess
 
 import pytest
 
+# pylint: disable=missing-format-argument-key
 
-def invoke_command(command: str):
-    return subprocess.call(command.strip().split(' '))
-
-
+SCENEDETECT_CMD = 'python -m scenedetect'
 VIDEO_PATH = 'tests/goldeneye/goldeneye.mp4'
-SCENEDETECT_CMD = 'python -m scenedetect '
-SET_TIME = 'time -s 2s -d 6s '    # Seek forward a bit but limit the amount we process.
-DEFAULT_DETECTOR = 'detect-content '
-GE_CMD = SCENEDETECT_CMD + '-i ' + VIDEO_PATH + ' '
-GE_STATS_CMD = GE_CMD + '-s goldeneye.csv ' + SET_TIME
-
+DEFAULT_STATSFILE = 'statsfile.csv'
+DEFAULT_TIME = '-s 2s -d 6s'    # Seek forward a bit but limit the amount we process.
+DEFAULT_DETECTOR = 'detect-content'
 ALL_DETECTORS = ['detect-content', 'detect-adaptive', 'detect-threshold']
 
 
+def invoke_scenedetect(args: str = '', **kwargs):
+    """Invokes the scenedetect CLI with the specified arguments and returns the exit code.
+    The kwargs are passed to the args format method, for example:
+        invoke_scenedetect('-i {VIDEO} {DETECTOR}', video='file.mp4', detector='detect-content')
+
+    Also sets the following template arguments to default values if present in args:
+        VIDEO -> VIDEO_PATH
+        DETECTOR -> DEFAULT_DETECTOR
+        TIME -> DEFAULT_TIME
+        STATS -> DEFAULT_STATSFILE
+    """
+    value_dict = dict(
+        VIDEO=VIDEO_PATH, TIME=DEFAULT_TIME, DETECTOR=DEFAULT_DETECTOR, STATS=DEFAULT_STATSFILE)
+    value_dict.update(**kwargs)
+    command = '{COMMAND} {ARGS}'.format(COMMAND=SCENEDETECT_CMD, ARGS=args.format(**value_dict))
+    return subprocess.call(command.strip().split(' '))
+
+
 def test_cli_no_args():
-    assert invoke_command(SCENEDETECT_CMD) == 0
+    assert invoke_scenedetect() == 0
 
 
 @pytest.mark.parametrize('info_command', ['help', 'about', 'version'])
 def test_cli_info_commands(info_command):
-    for command in ['help', 'version', 'about']:
-        assert invoke_command(SCENEDETECT_CMD + command) == 0
+    assert invoke_scenedetect(info_command) == 0
 
 
 @pytest.mark.parametrize('detector_command', ALL_DETECTORS)
 def test_cli_detectors(detector_command: str):
     # Ensure all detectors work with and without a statsfile.
-    assert invoke_command(GE_CMD + SET_TIME + detector_command) == 0
-    # Run with a statsfile twice to ensure the file is populated with those metrics and re-loaded.
-    assert invoke_command(GE_STATS_CMD + detector_command) == 0
-    assert invoke_command(GE_STATS_CMD + detector_command) == 0
+    assert invoke_scenedetect('-i {VIDEO} time {TIME} {DETECTOR}', DETECTOR=detector_command) == 0
+    # Run with a statsfile twice to ensure the file is populated with those metrics and reloaded.
+    assert invoke_scenedetect(
+        '-i {VIDEO} -s {STATS} time {TIME} {DETECTOR}', DETECTOR=detector_command) == 0
+    assert invoke_scenedetect(
+        '-i {VIDEO} -s {STATS} time {TIME} {DETECTOR}', DETECTOR=detector_command) == 0
 
 
 def test_cli_time():
     # TODO: Add test for timecode formats.
-    base_command = GE_CMD + DEFAULT_DETECTOR + 'time '
+    base_command = '-i {VIDEO} time {TIME} {DETECTOR}'
     # Test setting start, end, and duration.
-    assert invoke_command(base_command + '-s 2s -e 8s') == 0    # start/end
-    assert invoke_command(base_command + '-s 2s -d 6s') == 0    # start/duration
+    assert invoke_scenedetect(base_command, TIME='-s 2s -e 8s') == 0    # start/end
+    assert invoke_scenedetect(base_command, TIME='-s 2s -d 6s') == 0    # start/duration
     # Ensure cannot set end and duration at the same time.
-    assert invoke_command(base_command + '-s 2s -d 6s -e 8s') != 0    # start/duration
+    assert invoke_scenedetect(base_command, TIME='-s 2s -d 6s -e 8s') != 0
+    assert invoke_scenedetect(base_command, TIME='-s 2s -e 8s -d 6s ') != 0
 
 
 def test_cli_list_scenes():
-    base_command = GE_STATS_CMD + DEFAULT_DETECTOR + 'list-scenes '
-    assert invoke_command(base_command) == 0
-    assert invoke_command(base_command + '-n') == 0    # no output file
-    # TODO: Check for existence of scene list file, remove after.
+    # Regular invocation (TODO: Check for output file!)
+    assert invoke_scenedetect('-i {VIDEO} time {TIME} {DETECTOR} list-scenes') == 0
+    # Add statsfile
+    assert invoke_scenedetect('-i {VIDEO} -s {STATS} time {TIME} {DETECTOR} list-scenes') == 0
+    # Suppress output file
+    assert invoke_scenedetect('-i {VIDEO} time {TIME} {DETECTOR} list-scenes -n') == 0
 
 
 @pytest.mark.skip(reason="TODO(v1.0): Command not functional yet.")
 def test_cli_split_video():
-    base_command = GE_STATS_CMD + DEFAULT_DETECTOR + 'split-video '
-    assert invoke_command(base_command) == 0
+    assert invoke_scenedetect('-i {VIDEO} -s {STATS} time {TIME} {DETECTOR} split-video') == 0
     # TODO: Check for existence of split video files, remove after.
 
 
 def test_cli_save_images():
-    base_command = GE_STATS_CMD + DEFAULT_DETECTOR + 'save-images '
-    assert invoke_command(base_command) == 0
+    assert invoke_scenedetect('-i {VIDEO} -s {STATS} time {TIME} {DETECTOR} save-images') == 0
     # TODO: Check for existence of images, remove after.
 
 
 @pytest.mark.skip(reason="TODO(v1.0): Command not functional yet.")
 def test_cli_export_html():
-    base_command = GE_STATS_CMD + DEFAULT_DETECTOR
-    assert invoke_command(base_command + 'save-images export-html') == 0
-    assert invoke_command(base_command + 'export-html --no-images') == 0
+    base_command = '-i {VIDEO} -s {STATS} time {TIME} {DETECTOR} {COMMAND}'
+    assert invoke_scenedetect(base_command, COMMAND='save-images export-html') == 0
+    assert invoke_scenedetect(base_command, COMMAND='export-html --no-images') == 0
     # TODO: Check for existence of HTML & image files, remove after.
